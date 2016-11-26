0926c97025489739daf8fc85368677636a50b930
arnold134777 <512509373@qq.com>
2016-11-26 13:10:05 +0800

    Add Next Permutation

---
+++ javascript/Next Permutation/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+  if(nums.length <= 0){
+    return nums;
+  }
+  var length = nums.length - 1;
+  for()
+};
+
+var result = fourSum([1, 0, -1, 0, -2, 2],0);
+console.log(result);
